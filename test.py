0720fb838297b650a59939d1360dd0664a3f5c85
2201985 <114548316+2201985@users.noreply.github.com>
2023-05-09 22:05:40 +0800

    test.py

--- test.py
+++ test.py
@@ -4,20 +4,20 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    # Modify the message data
-    data.data = "Cliven"
-    # Publish the modified message to the 'name' topic
-    pub.publish(data)
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-if name == 'main':
-    # Initialize the node
-    rospy.init_node('changeMSG', anonymous=True)
+    # Modify the message
+    modified_str = "Modified: " + data.data
 
-    # Subscribe to the 'chatter' topic and set the callback function
+    # Publish the modified message
+    pub = rospy.Publisher('chatter_modified', String, queue_size=10)
+    pub.publish(modified_str)
+
+def message_modifier():
+    rospy.init_node('message_modifier', anonymous=True)
     rospy.Subscriber("chatter", String, callback)
 
-    # Publish to the 'name' topic
-    pub = rospy.Publisher('name', String, queue_size=10)
-
-    # Prevent the node from exiting
     rospy.spin()
+
+if __name__ == '__main__':
+    message_modifier()
