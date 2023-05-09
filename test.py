834f3f9e1b60467d0517ef68171c8de066331ffe
2201985 <114548316+2201985@users.noreply.github.com>
2023-05-09 22:00:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
     # Modify the message
-    change_str = "content: " + data.data
+    modified_str = "Modified: " + data.data
 
     # Publish the modified message
     pub = rospy.Publisher('chatter_modified', String, queue_size=10)
-    pub.publish(change_str)
+    pub.publish(modified_str)
 
 def message_modifier():
     rospy.init_node('message_modifier', anonymous=True)
